d8e3ab5fc37da4232d36b7f7b98b001f577db092
MahdiDev78 <MahdiDev78@gmail.com>
2021-10-03 08:56:54 +0330

    created demo

--- app/src/main/java/com/mahdikh/arrowpanel/MainActivity.kt
+++ app/src/main/java/com/mahdikh/arrowpanel/MainActivity.kt
@@ -2,7 +2,6 @@ package com.mahdikh.arrowpanel
 
 import android.graphics.Color
 import android.os.Bundle
-import android.view.Gravity
 import android.view.animation.OvershootInterpolator
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -10,19 +9,25 @@ import com.mahdikh.vision.arrowpanel.animator.ScaleAnimator
 import com.mahdikh.vision.arrowpanel.widget.ArrowPanel
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var arrowPanel: ArrowPanel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.btn).setOnClickListener {
-            arrowPanel = ArrowPanel.Builder(this)
+        findViewById<Button>(R.id.btn).setOnClickListener { view ->
+            ArrowPanel.Builder(this)
                 .setContentView(R.layout.panel)
                 .setDim(Color.BLACK, 0.55F)
-                .setAnimator(ScaleAnimator().setInterpolator(OvershootInterpolator()).setDuration(250))
-                .setTargetView(it)
+                .setAnimator(
+                    ScaleAnimator()
+                        .setInterpolator(OvershootInterpolator())
+                        .setDuration(250)
+                )
+                .setTargetView(view)
                 .setDrawTargetView(true)
                 .setCreateAsWindow(true)
+                .setBlurRadius(5F)
+                .setBlurQuality(30)
+                .setDrawBlurEffect(true)
                 .show()
         }
     }
