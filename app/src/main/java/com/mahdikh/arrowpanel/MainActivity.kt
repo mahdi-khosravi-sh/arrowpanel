ca429f540161e442f54ea44cd8dcc237e4256b09
MahdiDev78 <MahdiDev78@gmail.com>
2021-10-08 11:01:49 +0330

    update activity

--- app/src/main/java/com/mahdikh/arrowpanel/MainActivity.kt
+++ app/src/main/java/com/mahdikh/arrowpanel/MainActivity.kt
@@ -2,32 +2,24 @@ package com.mahdikh.arrowpanel
 
 import android.graphics.Color
 import android.os.Bundle
-import android.view.animation.OvershootInterpolator
+import android.view.Gravity
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.mahdikh.vision.arrowpanel.animator.ScaleAnimator
+import com.mahdikh.vision.arrowpanel.animator.SlideAnimator
 import com.mahdikh.vision.arrowpanel.widget.ArrowPanel
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewById<Button>(R.id.btn).setOnClickListener { view ->
             ArrowPanel.Builder(this)
                 .setContentView(R.layout.panel)
-                .setDim(Color.BLACK, 0.55F)
-                .setAnimator(
-                    ScaleAnimator()
-                        .setInterpolator(OvershootInterpolator())
-                        .setDuration(250)
-                )
+                .setAnimator(SlideAnimator(Gravity.START))
+                .setDim(Color.BLACK, 0.6F)
                 .setTargetView(view)
-                .setDrawTargetView(true)
-                .setCreateAsWindow(true)
-                .setBlurRadius(5F)
-                .setBlurQuality(30)
-                .setDrawBlurEffect(true)
+                .setPivotToArrow(false)
+                .setType(ArrowPanel.TYPE_DECOR)
                 .show()
         }
     }
