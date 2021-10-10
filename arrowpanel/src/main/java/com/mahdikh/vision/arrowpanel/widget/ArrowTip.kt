c0f802712e78c07c784f3d9205806d0189b1b761
MahdiDev78 <MahdiDev78@gmail.com>
2021-10-11 00:24:22 +0330

    fixed bug

--- arrowpanel/src/main/java/com/mahdikh/vision/arrowpanel/widget/ArrowTip.kt
+++ arrowpanel/src/main/java/com/mahdikh/vision/arrowpanel/widget/ArrowTip.kt
@@ -13,7 +13,7 @@ class ArrowTip private constructor(context: Context) : ArrowPanel(context) {
 
     init {
         setDim(Color.BLACK, 0.0f)
-        setInteractionTouchOutside(true)
+        interactionTouchOutside = true
         val view = setContentView(R.layout.arrowtip_simple_text)
         textView = view.findViewById(R.id.textView)
 
